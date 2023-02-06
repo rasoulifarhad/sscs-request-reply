44e79f0a70d49c4a3b5bdfaa45c2509592f19935
Farhad rasouli <rasouli.farhad@gmail.com>
2023-02-06 16:40:35 +0330

    change to flux

--- src/main/java/com/farhad/example/sscs/requestreply/controller/ClientController.java
+++ src/main/java/com/farhad/example/sscs/requestreply/controller/ClientController.java
@@ -1,8 +1,6 @@
 package com.farhad.example.sscs.requestreply.controller;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,7 +12,6 @@ import java.util.function.Function;
 
 @RequestMapping(path = "/api", produces = MediaType.TEXT_PLAIN_VALUE)
 @RestController
-@Slf4j
 public class ClientController {
 
     @Autowired
@@ -25,13 +22,5 @@ public class ClientController {
 
         return  convertSendAndReceive.apply(Flux.just(message));
 
-        // try {
-        //     String response = convertSendAndReceive.apply(message);
-        //     log.info("response : " + response);
-        //     return new ResponseEntity<>(response, HttpStatus.OK);
-        // } catch (Exception exception) {
-        //     log.error(exception.getMessage());
-        //     return new ResponseEntity<>(exception.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
-        // }
     }
 }
